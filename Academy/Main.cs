4a9ebda55527a924279139aaf5a551db5de48ff2
Devya_yi23@top-academy.ru <Devya_yi23@top-academy.ru>
2025-04-09 11:57:11 +0500

    DONE

--- Academy/Main.cs
+++ Academy/Main.cs
@@ -20,16 +20,69 @@ namespace Academy
 		Connector connector;
 
 		Dictionary<string, int> d_directions;
+		Dictionary<string, int> d_groups;
+
+		DataGridView[] tables;
+		Query[] queries = new Query[]
+			{
+				new Query
+				(
+					"last_name,first_name,middle_name,birth_date,group_name,direction_name",
+					"Students JOIN Groups ON Students.[group] = Groups.group_id " +
+					"JOIN Directions ON Groups.direction = Directions.direction_id"
+				),
+				new Query
+				(
+					"group_name,dbo.GetLearningDaysFor(group_name) AS weekdays,start_time,direction_name",
+					"Groups,Directions",
+					"direction=direction_id"
+				),
+				new Query
+				(
+					"direction_name,COUNT(DISTINCT group_id) AS N'Количество групп' , COUNT(stud_id) AS N'Количество студентов'",
+					"Students RIGHT JOIN Groups ON([group]=group_id) RIGHT JOIN Directions ON(direction=direction_id)",
+					"",
+					"direction_name"
+				),
+				new Query("*", "Disciplines"),
+				new Query("*", "Teachers")
+			};
+
+		string[] status_messages = new string[]
+			{
+				"Количество студентов: ",
+				"Количество групп: ",
+				"Количество направлений: ",
+				"Количество дисциплин: ",
+				"Количество преподавателей: ",
+			};
+
 		public Main()
 		{
 			InitializeComponent();
+
+			tables = new DataGridView[]
+			{
+				dgvStudents,
+				dgvGroups,
+				dgvDirections,
+				dgvDisciplines,
+				dgvTeachers
+			};
 
 			connector = new Connector
 				(
 					ConfigurationManager.ConnectionStrings["PV_319_Import"].ConnectionString
 				);
 			d_directions = connector.GetDictionary("*", "Directions");
-			cbGroupsDirection.Items.AddRange(d_directions.Select(k => k.Key).ToArray());
+			d_groups = connector.GetDictionary("group_id,group_name", "groups");
+			cbStudentsGroup.Items.AddRange(d_groups.Select(g => g.Key).ToArray());
+			cbGroupsDirection.Items.AddRange(d_directions.Select(d => d.Key).ToArray());
+			cbStudentsDirection.Items.AddRange(d_directions.Select(d => d.Key).ToArray());
+			cbStudentsGroup.Items.Insert(0, "Все группы");
+			cbStudentsDirection.Items.Insert(0, "Все направления");
+			cbStudentsGroup.SelectedIndex = cbStudentsDirection.SelectedIndex =  0;
+
 			//dgv - DataGridView
 			dgvStudents.DataSource = connector.Select
 					(
@@ -38,62 +91,70 @@ namespace Academy
 						"[group]=group_id AND direction=direction_id"
 					);
 			toolStripStatusLabelCount.Text = $"Количество студентов:{dgvStudents.RowCount - 1}.";
+			LoadPage(0);
 		}
-
+		void LoadPage(int i, Query query = null)
+		{
+			if (query == null) query = queries[i];
+			tables[i].DataSource = connector.Select(query.Columns, query.Tables, query.Condition, query.Group_by);
+			toolStripStatusLabelCount.Text = status_messages[i] + CountRecordsInDGV(tables[i]);
+		}
 		private void tabControl_SelectedIndexChanged(object sender, EventArgs e)
 		{
-			switch (tabControl.SelectedIndex)
-			{
-				case 0:
-					dgvStudents.DataSource = connector.Select
-				(
-					"last_name,first_name,middle_name,birth_date,group_name,direction_name",
-					"Students,Groups,Directions",
-					"[group]=group_id AND direction=direction_id"
-				);
-					toolStripStatusLabelCount.Text = $"Количество студентов: {dgvStudents.RowCount - 1}.";
-					break;
+			//int i = tabControl.SelectedIndex;
+			LoadPage(tabControl.SelectedIndex);
+			//switch (tabControl.SelectedIndex)
+			//{
+			//	case 0:
+			//		dgvStudents.DataSource = connector.Select
+			//	(
+			//		"last_name,first_name,middle_name,birth_date,group_name,direction_name",
+			//		"Students,Groups,Directions",
+			//		"[group]=group_id AND direction=direction_id"
+			//	);
+			//		toolStripStatusLabelCount.Text = $"Количество студентов: {dgvStudents.RowCount - 1}.";
+			//		break;
 
-				case 1:
+			//	case 1:
 
-					dgvGroups.DataSource = connector.Select
-						(
-							"group_name,dbo.GetLearningDaysFor(group_name) AS weekdays,start_time,direction_name",
-							"Groups,Directions",
-							"direction=direction_id"
-						);
-					toolStripStatusLabelCount.Text = $"Количество групп:{dgvGroups.RowCount - 1}.";
-					break;
+			//		dgvGroups.DataSource = connector.Select
+			//			(
+			//				"group_name,dbo.GetLearningDaysFor(group_name) AS weekdays,start_time,direction_name",
+			//				"Groups,Directions",
+			//				"direction=direction_id"
+			//			);
+			//		toolStripStatusLabelCount.Text = $"Количество групп:{dgvGroups.RowCount - 1}.";
+			//		break;
 
-				case 2:
-					//dgvDirections.DataSource = connector.Select
-					//	(
-					//	"direction_name,COUNT(DISTINCT group_id) AS N'Количество групп' , COUNT(stud_id) AS N'Количество студентов'", 
-					//	"Students,Groups,Directions",
-					//	"[group]=group_id AND direction=direction_id",
-					//	"direction_name"
-					//	);
+			//	case 2:
+			//		//dgvDirections.DataSource = connector.Select
+			//		//	(
+			//		//	"direction_name,COUNT(DISTINCT group_id) AS N'Количество групп' , COUNT(stud_id) AS N'Количество студентов'", 
+			//		//	"Students,Groups,Directions",
+			//		//	"[group]=group_id AND direction=direction_id",
+			//		//	"direction_name"
+			//		//	);
 
-					dgvDirections.DataSource = connector.Select
-						(
-							"direction_name,COUNT(DISTINCT group_id) AS N'Количество групп' , COUNT(stud_id) AS N'Количество студентов'",
-							"Students RIGHT JOIN Groups ON([group]=group_id) RIGHT JOIN Directions ON(direction=direction_id)",
-							"",
-							"direction_name"
-						);
-					toolStripStatusLabelCount.Text = $"Количество направлений: {dgvDirections.RowCount - 1}.";
-					break;
+			//		dgvDirections.DataSource = connector.Select
+			//			(
+			//				"direction_name,COUNT(DISTINCT group_id) AS N'Количество групп' , COUNT(stud_id) AS N'Количество студентов'",
+			//				"Students RIGHT JOIN Groups ON([group]=group_id) RIGHT JOIN Directions ON(direction=direction_id)",
+			//				"",
+			//				"direction_name"
+			//			);
+			//		toolStripStatusLabelCount.Text = $"Количество направлений: {dgvDirections.RowCount - 1}.";
+			//		break;
 
-				case 3:
-					dgvDisciplines.DataSource = connector.Select("*", "Disciplines");
-					toolStripStatusLabelCount.Text = $"Количество дисциплин: {dgvDisciplines.RowCount - 1}.";
-					break;
+			//	case 3:
+			//		dgvDisciplines.DataSource = connector.Select("*", "Disciplines");
+			//		toolStripStatusLabelCount.Text = $"Количество дисциплин: {dgvDisciplines.RowCount - 1}.";
+			//		break;
 
-				case 4:
-					dgvTeachers.DataSource = connector.Select("*", "Teachers");
-					toolStripStatusLabelCount.Text = $"Количество преподавателей: {dgvTeachers.RowCount - 1}.";
-					break;
-			}
+			//	case 4:
+			//		dgvTeachers.DataSource = connector.Select("*", "Teachers");
+			//		toolStripStatusLabelCount.Text = $"Количество преподавателей: {dgvTeachers.RowCount - 1}.";
+			//		break;
+			//}
 		}
 
 		private void cbGroupsDirection_SelectedIndexChanged(object sender, EventArgs e)
@@ -110,6 +171,25 @@ namespace Academy
 
 		{
 			return dgv.RowCount == 0 ? 0 : dgv.RowCount - 1;
+		}
+
+		private void cbStudentsDirection_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			int i = cbStudentsDirection.SelectedIndex;
+			Dictionary<string, int> d_groups = connector.GetDictionary
+			(
+				"group_id,group_name",
+				"Groups",
+				i==0 ? "" : $"direction={d_directions[cbStudentsDirection.SelectedItem.ToString()]}"
+			);
+			cbStudentsGroup.Items.Clear();
+			cbStudentsGroup.Items.AddRange(d_groups.Select(g => g.Key).ToArray());
+
+			Query query = new Query(queries[0]);
+			query.Condition = 
+				(i == 0 || cbStudentsDirection.SelectedItem == null ? "" : $"direction={d_directions[cbStudentsDirection.SelectedItem.ToString()]}");
+			LoadPage(0, query);
+
 		}
 	}
 }
